8e049332d60245d4a59ceb5284338a51272c57cf
angie88 <angieyeh24@gmail.com>
2016-01-22 16:51:14 -0800

    added body parser

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded());
 
 app.use(express.static(path.join(__dirname, './../')));
 app.get('/', function(){
@@ -9,4 +12,4 @@ app.get('/', function(){
 
 app.listen(3000, function() {
   console.log('Server is lisening on port 3000')
-}); 
+});
